31cc2fca699ddd92bebf74e35614fcd0f9a0e706
eyere7 <101259435+eyere7@users.noreply.github.com>
2022-03-20 05:20:23 -0700

    Add files via upload

---
+++ smart home automation code.ino
@@ -0,0 +1,31 @@
+char val;
+
+int light = 2; // RELAY VALUE
+
+void setup() {
+  pinMode(light, OUTPUT);
+  
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if (Serial.available()) {
+    val = Serial.read();
+    Serial.println();
+  }
+  if (val == '1')
+  { digitalWrite(light, HIGH);
+  Serial.println("LIGHT ON");
+
+  }
+  else if (val == '0')
+  {
+    digitalWrite(light, LOW);
+    Serial.println("LIGHT OFF");
+  }
+ //delay(1000);
+}
+
+
+
